3c8d7acd477a4a6ed72d63ef3dc06a4421cddcd1
soundarya001 <104191860+soundarya001@users.noreply.github.com>
2022-12-26 13:40:36 +0530

    Update index.php
    DOB CHANGED

--- index.php
+++ index.php
@@ -50,6 +50,12 @@ echo ' <div id="reg-head-fail" class="headrg">Registration Failed! <br> Error: '
 <td class="t-1"><div align="left" id="tb-name">Last&nbsp;Name:</div></td>
 <td><input type="text" name="lname" id="tb-box"/></td>
 </tr>
+ <tr>
+<td class="t-1"><div align="left" id="tb-name">DOB:</div></td>
+<td><input type="text" name="lname" id="tb-box"/></td>
+</tr>
+ 
+ 
 <tr>
 <td class="t-1"><div align="left" id="tb-name">Email:</div></td>
 <td><input type="text" id="tb-box" name="address" /></td>
